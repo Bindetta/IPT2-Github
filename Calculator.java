f2dd86c66cd667676042e7a12fb77fe32d16abd0
Bindetta <robingonzales122@gmail.com>
2024-09-12 14:29:58 +0800

    added java code

--- Calculator.java
+++ Calculator.java
@@ -1,1 +1,11 @@
-new file
+public class Caluclator{
+	public static void main(String[]args){
+		int a = 12;
+		int b = 4;
+	System.out.println("Addition" + add(a,b));
+	System.out.println("Subtraction " + sub(a,b));
+	System.out.println("Multiplication" + multi(a,b));
+	System.out.println("Division" + div(a,b));
+	System.out.println("Modulus" + mod(a,b));
+	}
+}
