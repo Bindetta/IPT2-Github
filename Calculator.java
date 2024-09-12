c01f1c1466ffdcae2f34c47d2be52e7c90060670
Bindetta <robingonzales122@gmail.com>
2024-09-12 15:02:39 +0800

    java code added

--- Calculator.java
+++ Calculator.java
@@ -2,25 +2,25 @@ public class Calculator{
 	public static void main(String[]args){
 		int a = 12;
 		int b = 4;
-	System.out.println("Addition" + add(a,b));
+	System.out.println("Addition " + add(a,b));
 	System.out.println("Subtraction " + sub(a,b));
-	System.out.println("Multiplication" + multi(a,b));
-	System.out.println("Division" + div(a,b));
-	System.out.println("Modulus" + mod(a,b));
+	System.out.println("Multiplication " + multi(a,b));
+	System.out.println("Division " + div(a,b));
+	System.out.println("Modulus " + mod(a,b));
 	}
-	public static int add(a, b){
+	public static int add(int a, int b){
 		return a + b;
 	}
-	public static int sub(a, b){
+	public static int sub(int a, int b){
 		return a - b;
 	}
-	public static int multi(a, b){
+	public static int multi(int a, int b){
 		return a * b;
 	}
-	public static int div(a, b){
+	public static int div(int a, int b){
 		return a / b;
 	}
-	public static int mod(a, b){
+	public static int mod(int a, int b){
 		return a % b;
 	}
 }
